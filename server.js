dad289e4de10cff6fb0732fffb9a2f6b2c1400f3
xzor9 <alexandrep@gmail.com>
2022-06-02 21:23:04 -0400

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const { request } = require('express')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 const rappers = {
     '21 savage':{
